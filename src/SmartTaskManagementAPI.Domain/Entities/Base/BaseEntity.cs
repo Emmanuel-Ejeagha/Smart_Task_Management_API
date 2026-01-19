9a177ca69f3d294ff6a4f386b27e0a31d7f4735d
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-01-19 18:46:17 +0100

    Add BaseEntity

---
+++ src/SmartTaskManagementAPI.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,32 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartTaskManagementAPI.Domain.Entities.Base;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    [NotMapped]
+    public List<BaseEvent> DomainEvents { get; } = new();
+
+    public void AddDomainEvent(BaseEvent domainEvent)
+    {
+        DomainEvents.Add(domainEvent);
+    }
+
+    public void RemoveDomainEvent(BaseEvent domainEvent)
+    {
+        DomainEvents.Remove(domainEvent);
+    }
+
+    public void ClearDomainEvent()
+    {
+        DomainEvents.Clear();
+    }
+}
+
+public abstract class BaseEvent
+{
+    public DateTime DateOccurred { get; protected set; } = DateTime.UtcNow;
+}
