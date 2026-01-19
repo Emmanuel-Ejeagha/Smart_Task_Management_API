de5988b5f90de57b4373429d740f5f2b7de9a028
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-01-19 18:44:44 +0100

    Add AuditableEntity

---
+++ src/SmartTaskManagementAPI.Domain/Entities/Base/AuditableEntity.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace SmartTaskManagementAPI.Domain.Entities.Base;
+
+public abstract class AuditableEntity : BaseEntity
+{
+    public DateTime CreatedAt { get; set; }
+    public Guid? CreatedBy { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public Guid? UpdatedBy { get; set; }
+    public bool IsDeleted { get; set; } = false;
+    public DateTime? DeletedAt { get; set; }
+    public Guid? DeletedBy { get; set; }
+
+    public void MarkAsCreated(Guid createdByUserId)
+    {
+        CreatedAt = DateTime.UtcNow;
+        CreatedBy = createdByUserId;
+    }
+
+    public void MarkAsUpdated(Guid updatedByUserId)
+    {
+        UpdatedAt = DateTime.UtcNow;
+        UpdatedBy = updatedByUserId;
+    }
+
+    public void MarkAsDeleted(Guid deletedByUserId)
+    {
+        IsDeleted = true;
+        DeletedAt = DateTime.UtcNow;
+        DeletedBy = deletedByUserId;
+    }
+
+    public void Restore()
+    {
+        IsDeleted = false;
+        DeletedAt = null;
+        DeletedBy = null;
+    }
+
+}
