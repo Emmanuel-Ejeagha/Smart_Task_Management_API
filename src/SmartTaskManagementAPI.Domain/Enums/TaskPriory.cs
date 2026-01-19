0d5726950b25f3da5263d9ff871fe378c62ff197
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-01-19 18:47:12 +0100

    Add Task Weight

---
+++ src/SmartTaskManagementAPI.Domain/Enums/TaskPriory.cs
@@ -0,0 +1,36 @@
+namespace SmartTaskManagementAPI.Domain.Enums;
+
+public enum TaskPriority
+{
+    Low = 1,
+    Meduim = 2,
+    High = 3,
+    Critical = 4
+}
+
+public static class TaskPrioryExxtensions
+{
+    public static string GetDisplayName(this TaskPriority priority)
+    {
+        return priority switch
+        {
+            TaskPriority.Low => "Low",
+            TaskPriority.Meduim => "Medium",
+            TaskPriority.High => "High",
+            TaskPriority.Critical => "Critical",
+            _ => priority.ToString()
+        };
+    }
+    
+    public static int GetWeight(this TaskPriority priority)
+    {
+        return priority switch
+        {
+            TaskPriority.Low => 1,
+            TaskPriority.Meduim => 2,
+            TaskPriority.High => 3,
+            TaskPriority.Critical => 4,
+            _ => 0
+        };
+    }
+}
