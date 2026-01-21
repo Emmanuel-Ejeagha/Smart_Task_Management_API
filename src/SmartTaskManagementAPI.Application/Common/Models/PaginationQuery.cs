b16ee2acc353e4bfe3d07fb259e0e251f3ef0790
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-01-21 23:21:53 +0100

    Add pagination query

---
+++ src/SmartTaskManagementAPI.Application/Common/Models/PaginationQuery.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace SmartTaskManagementAPI.Application.Common.Models;
+
+public class PaginationQuery
+{
+    private const int MaxPageSize = 100;
+    private int _pageSize = 10;
+
+    public int PageNumber { get; set; } = 1;
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+    public string? SortBy { get; set; }
+    public bool IsDescending { get; set; } = false;
+    public string? SearchTerm { get; set; }
+}
