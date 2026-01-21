1351a13180ad738f974de41d1a2e027d43f9ee4e
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-01-21 23:15:42 +0100

    Add AuthResponse Dto

---
+++ src/SmartTaskManagementAPI.Application/Features/Auth/DTOs/AuthResponse.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SmartTaskManagementAPI.Application.Features.Auth.DTOs;
+
+public class AuthResponse
+{
+    public string AccessToken { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
+    public DateTime ExpiredAt { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+    public Guid TenantId { get; set; }    
+}
