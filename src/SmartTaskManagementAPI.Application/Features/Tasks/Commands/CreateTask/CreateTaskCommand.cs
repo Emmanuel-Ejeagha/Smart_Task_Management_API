b5bd5816327734643164890475df11d06f6545f1
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-01-22 08:12:19 +0100

    Add Create Task Command

---
+++ src/SmartTaskManagementAPI.Application/Features/Tasks/Commands/CreateTask/CreateTaskCommand.cs
@@ -0,0 +1,15 @@
+using MediatR;
+using SmartTaskManagementAPI.Application.Features.Tasks.DTOs;
+using SmartTaskManagementAPI.Domain.Enums;
+
+namespace SmartTaskManagementAPI.Application.Features.Tasks.Commands.CreateTask;
+
+public class CreateTaskCommand : IRequest<TaskDto>
+{
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public TaskPriority Priority { get; set; } = TaskPriority.Meduim;
+    public DateTime? DueDate { get; set; }
+    public DateTime? ReminderDate { get; set; }
+
+}
