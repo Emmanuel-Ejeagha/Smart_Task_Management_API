d060fe200b6a73dcb2491f54e87aa6ed1565c629
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-01-28 20:49:40 +0100

    feat(background-jobs): add HangfireActivator for DI support

--- src/SmartTaskManagementAPI.Infrastructure/BackgroundJobs/HangfireActivator.cs
+++ src/SmartTaskManagementAPI.Infrastructure/BackgroundJobs/HangfireActivator.cs
@@ -1,4 +1,5 @@
 using Hangfire;
+using Hangfire.Server;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace SmartTaskManagementAPI.Infrastructure.BackgroundJobs;
@@ -9,36 +10,11 @@ public class HangfireActivator : JobActivator
 
     public HangfireActivator(IServiceProvider serviceProvider)
     {
-        _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+        _serviceProvider = serviceProvider;
     }
 
     public override object ActivateJob(Type jobType)
     {
         return _serviceProvider.GetRequiredService(jobType);
-    }
-
-    public override JobActivatorScope BeginScope(JobActivatorContext context)
-    {
-        return new HangfireActivatorScope(_serviceProvider.CreateAsyncScope());
-    }
-}
-
-public class HangfireActivatorScope : JobActivatorScope
-{
-    private readonly IServiceScope _serviceScope;
-
-    public HangfireActivatorScope(IServiceScope serviceScope)
-    {
-        _serviceScope = serviceScope ?? throw new ArgumentNullException(nameof(serviceScope));
-    }
-
-    public override object Resolve(Type type)
-    {
-        return _serviceScope.ServiceProvider.GetRequiredService(type);
-    }
-
-    public override void DisposeScope()
-    {
-        _serviceScope.Dispose();
     }
 }
