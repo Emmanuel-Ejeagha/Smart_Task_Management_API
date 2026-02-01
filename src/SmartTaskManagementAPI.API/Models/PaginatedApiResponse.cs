c8d8c27edea14916680e0248435192a868fe8a36
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-02-01 01:10:33 +0100

    feat:  pagination models

--- src/SmartTaskManagementAPI.API/Models/PaginatedApiResponse.cs
+++ src/SmartTaskManagementAPI.API/Models/PaginatedApiResponse.cs
@@ -2,27 +2,35 @@ using System;
 
 namespace SmartTaskManagementAPI.API.Models;
 
-public class PaginatedApiResponse<T> :ApiResponse<List<T>>
+public class PaginatedApiResponse<T> : ApiResponse<IEnumerable<T>>
 {
     public int PageNumber { get; set; }
     public int PageSize { get; set; }
-    public int TotalPages { get; set; }
     public int TotalCount { get; set; }
+    public int TotalPages { get; set; }
     public bool HasPreviousPage { get; set; }
     public bool HasNextPage { get; set; }
 
-    public PaginatedApiResponse(
-        List<T> data,
+    public static PaginatedApiResponse<T> SuccessResponse(
+        IEnumerable<T> data,
         int pageNumber,
         int pageSize,
         int totalCount,
-        string message = "") : base(data, message)
+        string message = "")
     {
-        PageNumber = pageNumber;
-        PageSize = pageSize;
-        TotalCount = totalCount;
-        TotalPages = (int)Math.Ceiling(totalCount / (double)pageNumber);
-        HasPreviousPage = pageNumber > 1;
-        HasNextPage = pageNumber < TotalPages;
-    }    
+        var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+        
+        return new PaginatedApiResponse<T>
+        {
+            Success = true,
+            Message = message,
+            Data = data,
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+            TotalCount = totalCount,
+            TotalPages = totalPages,
+            HasPreviousPage = pageNumber > 1,
+            HasNextPage = pageNumber < totalPages
+        };
+    }
 }
