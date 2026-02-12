3acf40c4a43dc3c9e171ac4105bf4689c076cc5b
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-02-12 21:22:22 +0100

    feat(api): add generic paginated response model

---
+++ src/SmartTaskManagement.API/Models/PaginatedResponse.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SmartTaskManagement.API.Models;
+
+public class PaginatedResponse<T>
+{
+    public IReadOnlyList<T> Items { get; set; } = new List<T>();
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages { get; set; }
+    public bool HasPreviousPage { get; set; }
+    public bool HasNextPage { get; set; }
+}
